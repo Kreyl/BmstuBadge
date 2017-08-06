ca2e0b46a262c9b3cd33948806e4f700f31b32fe
Kreyl <laystoll@yandex.ru>
2017-08-06 20:09:39 +0300

    Working FW project

---
+++ Firmware/main.cpp
@@ -0,0 +1,81 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 20 февр. 2014 г.
+ *      Author: g.kruglov
+ */
+
+#include "hal.h"
+#include "MsgQ.h"
+#include "kl_i2c.h"
+#include "Sequences.h"
+#include "shell.h"
+#include "led.h"
+#include "kl_sd.h"
+#include "kl_fs_utils.h"
+#include "SimpleSensors.h"
+
+// Forever
+EvtMsgQ_t<EvtMsg_t, MAIN_EVT_Q_LEN> EvtQMain;
+extern CmdUart_t Uart;
+void OnCmd(Shell_t *PShell);
+void ITask();
+
+LedRGB_t Led { LED_RED_CH, LED_GREEN_CH, LED_BLUE_CH };
+
+int main(void) {
+    // ==== Setup clock frequency ====
+    Clk.SetHiPerfMode();
+    Clk.Select48MhzSrc(src48PllQ);
+    Clk.UpdateFreqValues();
+
+    // Init OS
+    halInit();
+    chSysInit();
+    // ==== Init hardware ====
+    EvtQMain.Init();
+    Uart.Init(115200);
+    Printf("\r%S %S\r\n", APP_NAME, BUILD_TIME);
+    Clk.PrintFreqs();
+
+    Led.Init();
+    Led.StartOrRestart(lsqStart);
+
+    SD.Init();
+//    SimpleSensors::Init();
+
+    // Main cycle
+    ITask();
+}
+
+__noreturn
+void ITask() {
+    while(true) {
+        EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
+        switch(Msg.ID) {
+            case evtIdShellCmd:
+                OnCmd((Shell_t*)Msg.Ptr);
+                ((Shell_t*)Msg.Ptr)->SignalCmdProcessed();
+                break;
+
+            case evtIdButtons:
+//                Printf("Btn %u\r", Msg.BtnEvtInfo.BtnID);
+                break;
+
+            default: break;
+        } // switch
+    } // while true
+}
+
+#if 1 // ======================= Command processing ============================
+void OnCmd(Shell_t *PShell) {
+	Cmd_t *PCmd = &PShell->Cmd;
+//    __unused int32_t dw32 = 0;  // May be unused in some configurations
+//    Uart.Printf("\r%S\r", PCmd->Name);
+    // Handle command
+    if(PCmd->NameIs("Ping")) PShell->Ack(retvOk);
+    else if(PCmd->NameIs("Version")) PShell->Printf("%S %S\r", APP_NAME, BUILD_TIME);
+
+    else PShell->Ack(retvCmdUnknown);
+}
+#endif
